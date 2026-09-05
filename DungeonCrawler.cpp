5e843eeddb594b8b9859f38281afac44ca1ccca8
BrugLol <anastaciocelestino593@gmail.com>
2026-09-05 18:04:53 -0300

    Finished the movement, but will make some changes to it later

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -14,29 +14,55 @@ public:
         posy = y;
     }
 
-    void Move(vector<vector <char>> board, int ysize, int xsize) {
+    void Move(vector<vector <char>>& board, int ysize, int xsize) {
         char direction;
         cout << "What direction would you like to move? (w, a, s d) ";
 
         cin >> direction;
 
+        int newY;
+        int newX;
+
         if (direction == 'w' && posy-1 != 0) {
-            posy -= 1;
+            newY = posy - 1;
+
+            if (board[newY][posx] == '#') {
+                newY = posy;
+            } else {
+                posy = newY;
+            }
         }
         if (direction == 's' && posy+1 != ysize-1) {
-            posy += 1;
+            newY = posy + 1;
+
+            if (board[newY][posx] == '#') {
+                newY = posy;
+            } else {
+                posy = newY;
+            }
         }
         if (direction == 'a' && posx-1 != 0) {
-            posx -= 1;
+            newX = posx - 1;
+
+            if (board[posy][newX] == '#') {
+                newX = posx;
+            } else {
+                posx = newX;
+            }
         }
         if (direction == 'd' && posx+1 != xsize-1) {
-            posx += 1;
+            newX = posx + 1;
+
+            if (board[posy][newX] == '#') {
+                newX = posx;
+            } else {
+                posx = newX;
+            }
         }
     }
 };
 
-
-void DrawBoard(vector<vector <char>> board, int ysize, int xsize, Player p) {
+void GenerateBoard(vector<vector <char>>& board, int ysize, int xsize) {
     for (int col = 0; col < ysize; col++) {
         board.push_back({'#'});
 
@@ -50,8 +76,10 @@ void DrawBoard(vector<vector <char>> board, int ysize, int xsize, Player p) {
 
         board[col][xsize-1] = '#';
     }
+}
 
 
+void DrawBoard(vector<vector <char>> board, Player p) {
     board[p.posy][p.posx] = '@';
 
     for (auto& row : board) {
@@ -81,11 +109,14 @@ int main() {
     Player knight(middlex, middley);
     
 
+    GenerateBoard(board, ysize, xsize);
+
 
     // Game loop
     while (true) {
         system("cls");
-        DrawBoard(board, ysize, xsize, knight);
+        
+        DrawBoard(board, knight);
         knight.Move(board, ysize, xsize);
 
         cout << endl;
