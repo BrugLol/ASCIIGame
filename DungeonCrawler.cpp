e0715f37b062b06cb319d8fc707f98b7b2de5380
BrugLol <anastaciocelestino593@gmail.com>
2026-09-05 22:04:21 -0300

    Finished build

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <random>
 
 using namespace std;
+
+struct Position { //allows the easy modification of structs
+    int x;
+    int y;
+
+    Position(int ypos, int xpos) {
+        x = xpos;
+        y = ypos;
+    }
+};
+
+
 
 class Player {
 public:
@@ -62,7 +74,108 @@ public:
     }
 };
 
-void GenerateBoard(vector<vector <char>>& board, int ysize, int xsize) {
+
+
+int randomGen() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(1, 100);
+
+    int randomNum = dist(gen);
+    return randomNum;
+}
+
+Position randomPos(int ysize, int xsize, vector<vector <char>>& board) {
+    random_device rd;
+    mt19937 gen(rd());
+
+
+    
+    while (true) {
+        uniform_int_distribution<int> y(1, ysize-1);
+        uniform_int_distribution<int> x(1, xsize-1);
+        
+       int xp = x(gen);
+       int yp = y(gen);
+
+        if (board[yp][xp] == '.') {
+            Position p(yp, xp);
+
+            return p;
+        }
+    }
+    
+}
+
+bool IsDungeonClearable(vector<vector<char>> board, Player k, Position p) {
+    vector<Position> visitedtiles;
+    vector<Position> queue;
+
+    Position plrpos(k.posy, k.posx);
+
+    // Player can't start inside a wall
+    if (board[k.posy][k.posx] == '#') {
+        return false;
+    }
+
+    queue.push_back(plrpos);
+    visitedtiles.push_back(plrpos);
+
+    int i = 0;
+
+    while (i < queue.size()) {
+
+        Position current = queue[i];
+        i++;
+
+        // We reached the exit
+        if (current.x == p.x && current.y == p.y) {
+            return true;
+        }
+
+        // Four possible directions
+        int dx[] = { 0, 0, -1, 1 };
+        int dy[] = { -1, 1, 0, 0 };
+
+        for (int j = 0; j < 4; j++) {
+            int newX = current.x + dx[j];
+            int newY = current.y + dy[j];
+
+            // Make sure we're inside the board
+            if (newX < 0 || newX >= board[0].size() ||
+                newY < 0 || newY >= board.size()) {
+                continue;
+            }
+
+            // Don't walk through walls
+            if (board[newY][newX] == '#') {
+                continue;
+            }
+
+            Position next(newY, newX);
+
+            // Check if we've already visited this tile
+            bool alreadyVisited = false;
+
+            for (Position visited : visitedtiles) {
+                if (visited.x == next.x && visited.y == next.y) {
+                    alreadyVisited = true;
+                    break;
+                }
+            }
+
+            if (!alreadyVisited) {
+                visitedtiles.push_back(next);
+                queue.push_back(next);
+            }
+        }
+    }
+
+    // Explored everything and never found the exit
+    return false;
+}
+
+void GenerateBoard(vector<vector <char>>& board, int ysize, int xsize, Player k) {
     for (int col = 0; col < ysize; col++) {
         board.push_back({'#'});
 
@@ -70,13 +183,33 @@ void GenerateBoard(vector<vector <char>>& board, int ysize, int xsize) {
             if (col == 0 || col == ysize-1) {
                 board[col].push_back({'#'});
             } else {
-                board[col].push_back({'.'});
+                if (randomGen() <= 30) {
+                    board[col].push_back({'#'});
+                } else {
+                    board[col].push_back({'.'});
+                }
             }
         }
 
         board[col][xsize-1] = '#';
     }
+
+
+
+    Position rpos = randomPos(ysize, xsize, board); //get's the random pos
+
+    int rx = rpos.x;
+    int ry = rpos.y;
+
+    board[ry][rx] = '>';
+
+
+    if (!IsDungeonClearable(board, k, rpos)) {
+        board.clear();
+        GenerateBoard(board, ysize, xsize, k);
+    }
 }
+
 
 
 void DrawBoard(vector<vector <char>> board, Player p) {
@@ -109,7 +242,9 @@ int main() {
     Player knight(middlex, middley);
     
 
-    GenerateBoard(board, ysize, xsize);
+    GenerateBoard(board, ysize, xsize, knight);
+
+    
 
 
     // Game loop
